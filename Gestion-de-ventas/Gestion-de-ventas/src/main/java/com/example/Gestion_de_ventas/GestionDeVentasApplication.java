56435db676b857cd557b01653a2ddd74f0f8a7cb
xiomararomero98 <xio.romero@duocuc.cl>
2025-05-23 18:00:50 -0400

    gestion de ventas

---
+++ Gestion-de-ventas/Gestion-de-ventas/src/main/java/com/example/Gestion_de_ventas/GestionDeVentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_de_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeVentasApplication.class, args);
+	}
+
+}
---
+++ Gestion-de-ventas/Gestion-de-ventas/src/test/java/com/example/Gestion_de_ventas/GestionDeVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_de_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
